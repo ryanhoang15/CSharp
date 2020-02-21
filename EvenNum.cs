4d78d1a8bf627fc04613a5a65ba1652d31c342d1
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-02-21 13:05:02 -0800

    Add files via upload

---
+++ EvenNum.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace example1
+{
+    public class EvenNum
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter a Number : ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+                Console.Read();
+            }
+        }
+    }
+}
+
